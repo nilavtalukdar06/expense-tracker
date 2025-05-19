7957aba075ff19e22cd5baef49af3429e3e90bda
Nilav <nilavtalukdar06@gmail.com>
2025-05-19 12:39:23 +0530

    added google gen ai

---
+++ gemini/gemini.js
@@ -0,0 +1,17 @@
+import { GoogleGenAI } from "@google/genai";
+
+const ai = new GoogleGenAI({ apiKey: process.env.GEMINI_API_KEY });
+
+async function main(amount, category) {
+  const response = await ai.models.generateContent({
+    model: "gemini-2.0-flash",
+    contents: `Given the amount (${amount}) and the category (${category}), generate a realistic, concise, and contextually appropriate expense description that explains what the expense was for.`,
+    config: {
+      systemInstruction:
+        "You are a professional financial advisor. The user will provide an amount and an expense category. Based on this input, generate a concise and informative 20-25 word description summarizing the nature of the expense, its potential impact on personal finances, and whether it appears essential, discretionary, or avoidable.",
+    },
+  });
+  return response.text;
+}
+
+export default main;
