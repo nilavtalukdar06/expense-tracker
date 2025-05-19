8b7bbf03d6cc5d824625da87319848db3e8afdd9
Nilav <nilavtalukdar06@gmail.com>
2025-05-19 19:38:25 +0530

    added expense table (dummy)

--- app/(routes)/expenses/page.js
+++ app/(routes)/expenses/page.js
@@ -1,7 +1,21 @@
+import Navbar from "@/app/navbar";
+import ExpenseTable from "@/components/expense-table";
+
 export default function ExpensesPage() {
   return (
-    <div>
-      <h1>Expenses</h1>
-    </div>
+    <section>
+      <Navbar />
+      <div className="p-4 w-full text-center">
+        <h1 className="text-xl sm:text-2xl text-slate-600 font-medium">
+          Expenses
+        </h1>
+        <p className="max-w-lg mx-auto my-1 text-gray-500 text-sm">
+          All of your expenses till now
+        </p>
+      </div>
+      <div className="max-w-3xl mx-auto my-5">
+        <ExpenseTable />
+      </div>
+    </section>
   );
 }
---
+++ components/expense-table.jsx
@@ -0,0 +1,98 @@
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableFooter,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { Button } from "./ui/button";
+
+const invoices = [
+  {
+    invoice: "INV001",
+    paymentStatus: "Paid",
+    totalAmount: "$250.00",
+    paymentMethod: "Credit Card",
+  },
+  {
+    invoice: "INV002",
+    paymentStatus: "Pending",
+    totalAmount: "$150.00",
+    paymentMethod: "PayPal",
+  },
+  {
+    invoice: "INV003",
+    paymentStatus: "Unpaid",
+    totalAmount: "$350.00",
+    paymentMethod: "Bank Transfer",
+  },
+  {
+    invoice: "INV004",
+    paymentStatus: "Paid",
+    totalAmount: "$450.00",
+    paymentMethod: "Credit Card",
+  },
+  {
+    invoice: "INV005",
+    paymentStatus: "Paid",
+    totalAmount: "$550.00",
+    paymentMethod: "PayPal",
+  },
+  {
+    invoice: "INV006",
+    paymentStatus: "Pending",
+    totalAmount: "$200.00",
+    paymentMethod: "Bank Transfer",
+  },
+  {
+    invoice: "INV007",
+    paymentStatus: "Unpaid",
+    totalAmount: "$300.00",
+    paymentMethod: "Credit Card",
+  },
+];
+
+export default function ExpenseTable() {
+  return (
+    <section>
+      <Table>
+        <TableCaption>A list of your recent invoices.</TableCaption>
+        <TableHeader>
+          <TableRow>
+            <TableHead className="w-[100px]">Serial</TableHead>
+            <TableHead>Amount</TableHead>
+            <TableHead>Category</TableHead>
+            <TableHead>Description</TableHead>
+            <TableHead className="text-right">Action</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {invoices.map((invoice, index) => (
+            <TableRow key={invoice.invoice}>
+              <TableCell className="font-medium">{index + 1}</TableCell>
+              <TableCell>{invoice.invoice}</TableCell>
+              <TableCell>{invoice.paymentStatus}</TableCell>
+              <TableCell>
+                <Button className="bg-[#3fcf8e] border-[#34b27b] hover:bg-[#34b27b]">
+                  Reveal
+                </Button>
+              </TableCell>
+              <TableCell className="text-right">
+                <Button variant="destructive">Delete</Button>
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+        <TableFooter>
+          <TableRow>
+            <TableCell colSpan={4}>Total</TableCell>
+            <TableCell className="text-right">&#8377;2,500.00</TableCell>
+          </TableRow>
+        </TableFooter>
+      </Table>
+    </section>
+  );
+}
