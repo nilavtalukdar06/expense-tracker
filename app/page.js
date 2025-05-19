1708c5e60593375d4864944ab1601ae9de945d85
Nilav <nilavtalukdar06@gmail.com>
2025-05-19 11:07:53 +0530

    updated homepage

--- app/page.js
+++ app/page.js
@@ -4,7 +4,14 @@ export default function Home() {
   return (
     <section>
       <Navbar />
-      <h1 className="my-2">Expensify</h1>
+      <div className="w-full p-4">
+        <h1 className="text-xl sm:text-2xl text-slate-600 font-medium">
+          Expensify
+        </h1>
+        <p className="max-w-lg my-1 text-gray-500 text-sm">
+          Add and manage your expenses at one place
+        </p>
+      </div>
     </section>
   );
 }
